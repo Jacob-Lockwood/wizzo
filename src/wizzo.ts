effd5990df8fa0c454b8402fdf132e11c6883696
skra-pa-pa <68607796+skra-pa-pa@users.noreply.github.com>
2022-03-26 08:59:04 -0400

    Added prettier config

--- src/wizzo.ts
+++ src/wizzo.ts
@@ -1,14 +1,14 @@
-import { EvtListener, Props, Style } from "./wizzoPropTypes";
+import { EvtListener, Props, Style } from "./wizzoPropTypes"
 
-type WizzoElement = HTMLElement | DocumentFragment;
+type WizzoElement = HTMLElement | DocumentFragment
 type WizzoChild =
   | string
   | WizzoElement
-  | ((content: (child: HTMLElement) => void) => void);
+  | ((content: (child: HTMLElement) => void) => void)
 type WizzoTaggedTemplate = [
   strings: TemplateStringsArray,
   ...values: Array<WizzoChild | WizzoChild[]>
-];
+]
 
 const WizzoTaggedTemplateToWizzoChildrenArr = ([
   strings,
@@ -20,18 +20,18 @@ const WizzoTaggedTemplateToWizzoChildrenArr = ([
       (array, value, index) => [...array, value, strings[index + 1]],
       [strings[0]]
     )
-    .filter(Boolean);
+    .filter(Boolean)
 
 const templateToChildren = (templateString: WizzoTaggedTemplate) =>
-  WizzoTaggedTemplateToWizzoChildrenArr(templateString).map((child) => {
-    if (typeof child !== "function") return child;
-    let temp: HTMLElement = $("div")()``;
-    child((newChild) => {
-      temp.replaceWith(newChild);
-      temp = newChild;
-    });
-    return temp;
-  });
+  WizzoTaggedTemplateToWizzoChildrenArr(templateString).map(child => {
+    if (typeof child !== "function") return child
+    let temp: HTMLElement = $("div")()``
+    child(newChild => {
+      temp.replaceWith(newChild)
+      temp = newChild
+    })
+    return temp
+  })
 
 const isListener = <
   T1 extends keyof HTMLElementEventMap,
@@ -39,33 +39,33 @@ const isListener = <
 >(
   key: string,
   value: any
-): value is EvtListener<T1, T2> => key.slice(0, 2) === "on";
-const isStyle = (key: string, value: any): value is Style => key === "style";
+): value is EvtListener<T1, T2> => key.slice(0, 2) === "on"
+const isStyle = (key: string, value: any): value is Style => key === "style"
 
 export default function $<T extends keyof HTMLElementTagNameMap>(tagName: T) {
   return (props?: Props<T>) =>
     (...templateString: WizzoTaggedTemplate) => {
-      const ele = document.createElement(tagName);
-      ele.append(...templateToChildren(templateString));
+      const ele = document.createElement(tagName)
+      ele.append(...templateToChildren(templateString))
       if (props)
         for (const [key, value] of Object.entries(props)) {
           if (isStyle(key, value))
             for (const [prop, val] of Object.entries(value)) {
-              ele.style.setProperty(prop, val as string);
+              ele.style.setProperty(prop, val as string)
             }
           else if (isListener(key, value))
             ele.addEventListener(
               key.slice(2).toLowerCase(),
               value as EventListener
-            );
-          else if (typeof value === "string") ele.setAttribute(key, value);
+            )
+          else if (typeof value === "string") ele.setAttribute(key, value)
         }
-      return ele;
-    };
+      return ele
+    }
 }
 
 export const frag = (...templateString: WizzoTaggedTemplate) => {
-  const fragment = new DocumentFragment();
-  fragment.append(...templateToChildren(templateString));
-  return fragment;
-};
+  const fragment = new DocumentFragment()
+  fragment.append(...templateToChildren(templateString))
+  return fragment
+}
